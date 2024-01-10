c3eec35a863787059d6f3b676488fd9405f040d9
xwhoknowz <jeremiah2498@gmail.com>
2024-01-10 14:46:18 -0600

    added form event listeners that pull event data based on user submissions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,55 @@
-var submitBtn = document.getElementById('submitBtn')
+var artistForm = document.getElementById('artist-search')
+var artistInput = document.getElementById(`artist-input`)
+var cityStateForm = document.getElementById(`city-state-search`)
+var cityInput = document.getElementById(`city-input`)
+var stateInput = document.getElementById(`state-input`)
 var eName = document.getElementById('eName');
-var eDateTime = doucment.getElementById('eDateTime');
+var eDateTime = document.getElementById('eDateTime');
 var eCity = document.getElementById('eCity');
 var eVenue = document.getElementById('eVenue');
 var artName = document.getElementById('artName');
 var artGenre = document.getElementById('artGenre');
 var artRecent = document.getElementById('artRecent');
-
 var ticketMasterKey = `6Y9ApEUv7D1XBXY1QjDD3Y6TwozbNAik`
-var artistEvents = `https://app.ticketmaster.com/discovery/v2/events?apikey=${ticketMasterKey}&keyword=J.%20Cole=*&sort=date,asc&classificationName=music`
 // TODO: If time permits add city search functionality
 // var cityEvents = `https://app.ticketmaster.com/discovery/v2/events?apikey=${ticketMasterKey}&locale=*&sort=date,asc&city=Dallas&stateCode=TX&classificationName=music`
 
-fetch(artistEvents)
-.then(function (response) {
-    return response.json()
-})
-.then(function (data) {
-    console.log(data);
-})
+function artistSearch(event) {
+    event.preventDefault();
+    
+    var searchedArtist= artistInput.value.trim()
+    console.log(searchedArtist);
+    var artistEvents = `https://app.ticketmaster.com/discovery/v2/events?apikey=${ticketMasterKey}&keyword=${searchedArtist}&locale=*&sort=date,asc&classificationName=music`
+    
+
+    fetch(artistEvents)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (data) {
+        console.log(data);
+    })
+}
+
+function cityStateSearch(event) {
+    event.preventDefault();
+    
+    var searchedCity= cityInput.value.trim()
+    console.log(searchedCity);
+    var selectedState= stateInput.value
+    console.log(selectedState);
+    var locationEvents = `https://app.ticketmaster.com/discovery/v2/events?apikey=6Y9ApEUv7D1XBXY1QjDD3Y6TwozbNAik&locale=*&sort=date,asc&city=${searchedCity}&stateCode=${selectedState}&classificationName=music`
+    
+
+    fetch(locationEvents)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (data) {
+        console.log(data);
+    })
+}
+
 
 // TODO: If time permits add city search functionality
 // fetch(cityEvents)
@@ -28,3 +59,6 @@ fetch(artistEvents)
 // .then(function (data) {
 //     console.log(data);
 // })
+
+artistForm.addEventListener(`submit`, artistSearch)
+cityStateForm.addEventListener(`submit`, cityStateSearch)
