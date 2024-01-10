34bae7f29bbc1dd75c571864a07f173528f6b60c
xwhoknowz <jeremiah2498@gmail.com>
2024-01-10 11:25:07 -0600

    made contact w ticket master api, and received test data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
 console.log('hello world')
+var ticketMasterKey = `6Y9ApEUv7D1XBXY1QjDD3Y6TwozbNAik`
+var ticketMasterEvents = `http://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=${ticketMasterKey}`
+
+fetch(ticketMasterEvents)
+.then(function (response) {
+    return response.json()
+})
+.then(function (data) {
+    console.log(data);
+})
