c67604629f403b1462a25bb36ad49580e6d1e9fb
xwhoknowz <jeremiah2498@gmail.com>
2024-01-10 13:51:44 -0600

    created a fetch for event data based on artist

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,22 @@
 console.log('hello world')
 var ticketMasterKey = `6Y9ApEUv7D1XBXY1QjDD3Y6TwozbNAik`
-var ticketMasterEvents = `http://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=${ticketMasterKey}`
+var artistEvents = `https://app.ticketmaster.com/discovery/v2/events?apikey=${ticketMasterKey}&keyword=J.%20Cole=*&sort=date,asc&classificationName=music`
+// TODO: If time permits add city search functionality
+// var cityEvents = `https://app.ticketmaster.com/discovery/v2/events?apikey=${ticketMasterKey}&locale=*&sort=date,asc&city=Dallas&stateCode=TX&classificationName=music`
 
-fetch(ticketMasterEvents)
+fetch(artistEvents)
 .then(function (response) {
     return response.json()
 })
 .then(function (data) {
     console.log(data);
 })
+
+// TODO: If time permits add city search functionality
+// fetch(cityEvents)
+// .then(function (response) {
+//     return response.json()
+// })
+// .then(function (data) {
+//     console.log(data);
+// })
