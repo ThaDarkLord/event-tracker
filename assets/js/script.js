7acadd44a88d011dde25db91516754f7f97a62d4
xwhoknowz <jeremiah2498@gmail.com>
2024-01-12 14:01:04 -0600

    added logic from aritstSearch function to citySearch function so it would create elements, add content, and append to the page based on the city search data response

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,7 +47,7 @@ function artistSearch(event) {
             } else {
 
                 for (let i = 0; i < data._embedded.events.length; i++) {
-                    console.log(data._embedded.events[i]);
+                    // console.log(data._embedded.events[i]);
 
                     // *Container Article
                     var articleContainer = document.createElement(`article`)
@@ -91,15 +91,14 @@ function artistSearch(event) {
                     eventDtPara.textContent =
                         "Date/Time: " + data._embedded.events[i].dates.start.dateTime;
 
-                    if (data._embedded.events[i]._embedded.venues[0].state.stateCode) {
-                        eventCityPara.textContent = "City: " + data._embedded.events[i]._embedded.venues[0].city.name + ", " + data._embedded.events[i]._embedded.venues[0].state.stateCode;
+                    if (!data._embedded.events[i]._embedded.venues[0].state) {
+                        eventCityPara.textContent = "City: " + data._embedded.events[i]._embedded.venues[0].city.name 
                     }
                     else {
-                        eventCityPara.textContent = "City: " +
-                            data._embedded.events[i]._embedded.venues[0].city.name;
+                        eventCityPara.textContent = "City: " + data._embedded.events[i]._embedded.venues[0].city.name + ", " + data._embedded.events[i]._embedded.venues[0].state.stateCode;
                     }
                     eventVenuePara.textContent =
-                        "City: " + data._embedded.events[i]._embedded.venues[0].name;
+                    "Venue: " + data._embedded.events[i]._embedded.venues[0].name;
 
                     // }
 
@@ -112,15 +111,15 @@ function artistSearch(event) {
                     eventCityArticle.appendChild(eventCityHeader)
                     eventCityArticle.appendChild(eventCityPara)
 
+                    if(data._embedded.events[i]._embedded.venues[0].name){
                     eventVenueArticle.appendChild(eventVenueHeader)
                     eventVenueArticle.appendChild(eventVenuePara)
-
+                    } else{
+                        console.log(`No Venue`);
+                    }
                     articleContainer.append(eventNameArticle, eventDtArticle, eventCityArticle, eventVenueArticle)
 
                     eventFeed.append(articleContainer)
-
-
-
 
                 }
             }
@@ -141,18 +140,88 @@ function cityStateSearch(event) {
             return response.json();
         })
         .then(function (data) {
-            console.log(data);
+          
+            for (let i = 0; i < data._embedded.events.length; i++) {
+                // console.log(data._embedded.events[i]);
+
+                // *Container Article
+                var articleContainer = document.createElement(`article`)
+                articleContainer.setAttribute(`class`, `column is-8`)
+
+                // *Event Name Article
+                var eventNameArticle = document.createElement(`article`)
+                eventNameArticle.setAttribute(`class`, `card eName`)
+                var eventNameHeader = document.createElement(`header`)
+                eventNameHeader.setAttribute(`class`, `card-header`)
+                var eventNamePara = document.createElement(`p`)
+                eventNamePara.setAttribute(`class`, `card-header-title event-name`)
+
+
+                var eventDtArticle = document.createElement(`article`)
+                eventDtArticle.setAttribute(`class`, `card eName`)
+                var eventDtHeader = document.createElement(`header`)
+                eventDtHeader.setAttribute(`class`, `card-header`)
+                var eventDtPara = document.createElement(`p`)
+                eventDtPara.setAttribute(`class`, `card-header-title event-name`)
+
+
+                var eventCityArticle = document.createElement(`article`)
+                eventCityArticle.setAttribute(`class`, `card eName`)
+                var eventCityHeader = document.createElement(`header`)
+                eventCityHeader.setAttribute(`class`, `card-header`)
+                var eventCityPara = document.createElement(`p`)
+                eventCityPara.setAttribute(`class`, `card-header-title event-name`)
+
+                var eventVenueArticle = document.createElement(`article`)
+                eventVenueArticle.setAttribute(`class`, `card eName`)
+                var eventVenueHeader = document.createElement(`header`)
+                eventVenueHeader.setAttribute(`class`, `card-header`)
+                var eventVenuePara = document.createElement(`p`)
+                eventVenuePara.setAttribute(`class`, `card-header-title event-name`)
+
+
+                // if (data._embedded.events[i] == data._embedded.events[2]) {
+                eventNamePara.textContent =
+                    "Event Name: " + data._embedded.events[i].name;
+                eventDtPara.textContent =
+                    "Date/Time: " + data._embedded.events[i].dates.start.dateTime;
+
+                if (!data._embedded.events[i]._embedded.venues[0].state) {
+                    eventCityPara.textContent = "City: " + data._embedded.events[i]._embedded.venues[0].city.name 
+                }
+                else {
+                    eventCityPara.textContent = "City: " + data._embedded.events[i]._embedded.venues[0].city.name + ", " + data._embedded.events[i]._embedded.venues[0].state.stateCode;
+                }
+                eventVenuePara.textContent =
+                "Venue: " + data._embedded.events[i]._embedded.venues[0].name;
+
+                // }
+
+                eventNameArticle.appendChild(eventNameHeader)
+                eventNameArticle.appendChild(eventNamePara)
+
+                eventDtArticle.appendChild(eventDtHeader)
+                eventDtArticle.appendChild(eventDtPara)
+
+                eventCityArticle.appendChild(eventCityHeader)
+                eventCityArticle.appendChild(eventCityPara)
+
+                if(data._embedded.events[i]._embedded.venues[0].name){
+                eventVenueArticle.appendChild(eventVenueHeader)
+                eventVenueArticle.appendChild(eventVenuePara)
+                } else{
+                    console.log(`No Venue`);
+                }
+                articleContainer.append(eventNameArticle, eventDtArticle, eventCityArticle, eventVenueArticle)
+
+                eventFeed.append(articleContainer)
+
+            }
+
+
         });
 }
 
-// TODO: Add city search functionality
-// fetch(cityEvents)
-// .then(function (response) {
-//     return response.json()
-// })
-// .then(function (data) {
-//     console.log(data);
-// })
 
 artistForm.addEventListener(`submit`, artistSearch);
 cityStateForm.addEventListener(`submit`, cityStateSearch);
